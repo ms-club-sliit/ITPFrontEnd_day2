32ffef5a53c991f765471f871317f6e38923942f
MadhushaPrasad <madushaprasad21@gmail.com>
2023-02-12 08:10:32 +0530

    create routes for admin panel

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/admin",
+    name: "admin",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/admin/DashboardPage.vue"),
+    children: [
+      {
+        default: true,
+        path: "",
+        name: "",
+        component: () => import("@/views/admin/DashboardContent.vue"),
+        props: true,
+      },
+      {
+        path: "items",
+        name: "items",
+        component: () => import("@/views/admin/ItemPage.vue"),
+        props: true,
+      },
+      {
+        path: "customers",
+        name: "customers",
+        component: () => import("@/views/admin/CustomerPage.vue"),
+        props: true,
+      },
+      {
+        path: "orders",
+        name: "orders",
+        component: () => import("@/views/admin/OrderPage.vue"),
+        props: true,
+      },
+      {
+        path: "reports",
+        name: "reports",
+        component: () => import("@/views/admin/ReportPage.vue"),
+        props: true,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
