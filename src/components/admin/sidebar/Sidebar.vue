1420da98004e669392240035f3dfbcd1b054afd0
MadhushaPrasad <madushaprasad21@gmail.com>
2023-02-12 08:12:30 +0530

    create sidebar for admin panel

---
+++ src/components/admin/sidebar/Sidebar.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li>
+        <a href="/admin/">
+          <span class="icon"><i class="fa fa-tachometer" /></span>
+          <span class="title">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="/admin/customers">
+          <span class="icon"><i class="fa-solid fa-user-group" /></span>
+          <span class="title">Customer</span>
+        </a>
+      </li>
+      <li>
+        <a href="/admin/items">
+          <span class="icon"
+            ><i aria-hidden="true" class="fa fa-cutlery"
+          /></span>
+          <span class="title">Items</span>
+        </a>
+      </li>
+      <li>
+        <a href="/admin/orders">
+          <span class="icon"
+            ><i aria-hidden="true" class="fa fa-archive"
+          /></span>
+          <span class="title">Order</span>
+        </a>
+      </li>
+      <li>
+        <a href="/admin/reports">
+          <span class="icon"><i aria-hidden="true" class="fa fa-book" /></span>
+          <span class="title">Reports</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBarComponent",
+};
+</script>
+
+<style scoped>
+.wrapper .sidebar {
+  position: fixed;
+  top: 70px;
+  left: 10px;
+  background: #2cb93b;
+  width: 200px;
+  height: calc(100% - 80px);
+  border-bottom-left-radius: 20px;
+  transition: all 0.3s ease;
+  z-index: 1000;
+}
+
+.wrapper .sidebar ul li a {
+  display: block;
+  padding: 20px;
+  position: relative;
+  margin-bottom: 1px;
+  color: #000000;
+  white-space: nowrap;
+}
+
+.wrapper .sidebar ul li a:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 3px;
+  height: 100%;
+  background: #9a1b00;
+  display: none;
+}
+
+.wrapper .sidebar ul li a span.icon {
+  margin-right: 10px;
+  display: inline-block;
+}
+
+.wrapper .sidebar ul li a span.title {
+  display: inline-block;
+}
+
+.wrapper .sidebar ul li a:hover,
+.wrapper .sidebar ul li a.active {
+  background: linear-gradient(
+    133.59deg,
+    #e03312 -1.23%,
+    #e85435 50.67%,
+    #faa395 105.91%
+  );
+  color: #ffffff;
+  font-weight: bold;
+}
+
+.wrapper .sidebar ul li a:hover:before,
+.wrapper .sidebar ul li a.active:before {
+  display: block;
+}
+
+.wrapper.collapse .sidebar {
+  width: 70px;
+}
+
+.wrapper.collapse .sidebar ul li a {
+  text-align: center;
+}
+
+.wrapper.collapse .sidebar ul li a span.icon {
+  margin: 0;
+}
+
+.wrapper.collapse .sidebar ul li a span.title {
+  display: none;
+}
+
+ul {
+  padding-left: 0;
+}
+</style>
